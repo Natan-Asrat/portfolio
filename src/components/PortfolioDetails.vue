686aca0d37cca9937343267f0932304c906e4dc3
Natan-Asrat <nathanyilmaasrat@gmail.com>
2024-09-16 02:29:59 +0300

    home

--- src/components/PortfolioDetails.vue
+++ src/components/PortfolioDetails.vue
@@ -5,7 +5,7 @@
         <div class="container">
           <nav class="breadcrumbs">
             <ol>
-              <li><a href="index.html">Home</a></li>
+              <li><a href="/">Home</a></li>
               <li class="current">{{ pageTitle }}</li>
             </ol>
           </nav>
