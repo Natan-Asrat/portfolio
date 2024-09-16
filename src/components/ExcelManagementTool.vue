2e457836bc04cf17af421d37c52cd8e12a298d40
Natan-Asrat <nathanyilmaasrat@gmail.com>
2024-09-16 07:55:25 +0300

    details continued

--- src/components/ExcelManagementTool.vue
+++ src/components/ExcelManagementTool.vue
@@ -35,7 +35,7 @@
   '/desktop/excel_management/excel_mgt_search.jpg" title="Excel Management Tool" data-gallery="portfolio-gallery-desktop'
         ],
         portfolioTitle: 'Excel Management Tool',
-        portfolioDescription: 'Our Excel Management Tool allows you to index your spreadsheets by any specific column, such as names, and store both the values and files in an easily searchable database.<br> Instantly find the exact file you need, along with its folder structure, with just a few clicks.',
+        portfolioDescription: 'Our Excel Management Tool allows you to index your spreadsheets by any specific column, such as names, and store both the values and files in an easily searchable database. Instantly find the exact file you need, along with its folder structure, with just a few clicks.',
         portfolioAdditionalInfo: "The Eureka Excel Management Tool enables users to import all Excel files from a designated folder, and provides powerful search and indexing capabilities. Users can assign custom names to specific cells (e.g., changing cell A2 to 'Index'), and the tool will automatically identify and extract all data below the indexed cell, essentially capturing the entire column's values. This information, along with the file path and name, is stored in an SQLite database. The tool allows for efficient search across multiple spreadsheets, providing quick access to files based on the values found within them.",
         portfolioExtraDetails: 'No extra details.',
         portfolioCategory: 'Desktop Java Swing App | SQLite DB | VueJS Website',
