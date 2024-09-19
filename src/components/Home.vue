53b01c53330fc30438281d76d86ecf9f3907e18b
Natan-Asrat <nathanyilmaasrat@gmail.com>
2024-09-19 13:40:20 +0300

    650% correction

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -153,7 +153,7 @@
 
         
         <div class="progress">
-          <span class="skill"><span>Vue.js | HTML | CSS | JavaScript</span> <i class="val">650%</i></span>
+          <span class="skill"><span>Vue.js | HTML | CSS | JavaScript</span> <i class="val">65%</i></span>
           <div class="progress-bar-wrap">
             <div class="progress-bar" role="progressbar" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
           </div>
